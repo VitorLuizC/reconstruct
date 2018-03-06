c8e0b8c89d489901c983f3daa6bd6b992bd6f6a5
Vitor Cavalcanti <vitorluizc@outlook.com>
2018-03-06 16:17:15 -0300

    Add reconstruct ~broken

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import toEntries from 'object.entries'
 
-const getProperties = <T>(object: T) => toEntries(object).map(([ property ]) => property)
-const getValues = <T>(object: T) => toEntries(object).map(([ , value ]) => value)
+type ReconstructΛ = <T extends Object, U extends Object>(value: T[keyof T], property: keyof T, object: T) => U
 
-export { getProperties, getValues }
+const reconstruct = <T extends Object, U extends Object>(object: T, λ: ReconstructΛ) => {
+  const merge = (result: U, [ property, value ]: [keyof T, T[keyof T]]) => {
+    return Object.assign({}, result, λ(value, property, object))
+  }
+
+  const entries = toEntries(object)
+  const result = entries.reduce(merge, {})
+  return result
+}
+
+export { ReconstructΛ, reconstruct, reconstruct as default }
