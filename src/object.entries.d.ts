92fadd879d48ba6addfdd2cf8b3a6cb7397634d7
Vitor Cavalcanti <vitorluizc@outlook.com>
2018-03-07 10:47:10 -0300

    Add generic Entry type

--- src/object.entries.d.ts
+++ src/object.entries.d.ts
@@ -1,10 +1,11 @@
 declare module 'object.entries' {
+  type Entry<T> = [keyof T, T[keyof T]]
 
   /**
    * Returns an array of key/values of the enumerable properties of an object.
    * @param object Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
    */
-  function toEntries <T> (object: T): Array<[keyof T, T[keyof T]]>
+  function entries <T> (object: T): Array<Entry<T>>
 
-  export default toEntries
+  export default entries
 }
